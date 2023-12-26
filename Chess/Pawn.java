eec8c6a81bd41e06eef7b08b633486f532336999
David <zhongyi.david.li@gmail.com>
2023-12-26 23:14:01 +0000

    fixed bugs in pawn and in settings

--- Chess/Pawn.java
+++ Chess/Pawn.java
@@ -24,31 +24,38 @@ public class Pawn extends ChessPiece {
                 curRow + direction,
                 curCol
             );
+        
+        // If we're on the last rank we should be promoted
+        // once again, todo promotion
+        if (!Utils.isValidSquare(newPosition)) return moves;
 
         // Standard move one square forward
         if (board.pieceAt(newPosition) == null) {
             moves.add(new Move(this, this.position, newPosition, false));
 
-            // Move two squares forward if it's the pawn's first move
-            newPosition = new Pair(
-                curRow + direction * 2,
-                curCol
-            );
+            if (!this.hasMoved) {
+                // Move two squares forward if it's the pawn's first move
+                // because it's the pawn's first move, the target square must be valid.
+                newPosition = new Pair(
+                    curRow + direction * 2,
+                    curCol
+                );
 
-            if (
-                (!this.hasMoved) && 
-                (board.pieceAt(newPosition) == null)
-            ) {
-                moves.add(new Move(this, this.position, newPosition, false));
+                if (board.pieceAt(newPosition) == null) {
+                    moves.add(new Move(this, this.position, newPosition, false));
+                }
             }
         }
 
         // Captures
         for (int dCol : new int[] {-1, 1}) {
+
             newPosition = new Pair(
                 curRow + direction,
                 curCol + dCol
             );
+
+            if (!Utils.isValidSquare(newPosition)) continue;
 
             if (isEnemy(board.pieceAt(newPosition))) {
                 moves.add(new Move(this, this.position, newPosition, true));
--- Chess/Settings.java
+++ Chess/Settings.java
@@ -1,21 +1,29 @@
 public class Settings {
     public static final boolean lightMode = false;
     public static final String ws = " ", bs = " ";
-    public static final String startingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    public static final String startingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-    public static String getOverlaySeparator(boolean previousOccupied, boolean currentOccupied) {
+    public static String getOverlaySeparator(boolean previousOccupied, boolean currentOccupied, boolean whitesTurn) {
         // if previous is not occupied and current is not occupied, return " "
         // if previous is occupied and current is occupied, return "|"
         // if previous is not occupied and current is occupied, return "["
         // if previous is occupied and current is not occupied, return "]"
+        // XOR needed because the whole string is flipped when it's black's turn.
+        // so basically we print "]" if we want "[" and it's black's turn. 
         if (previousOccupied == false && currentOccupied == false)
             return " ";
-        if (previousOccupied == false && currentOccupied == true)
-            return "[";
-        if (previousOccupied == true && currentOccupied == false)
+        if (previousOccupied == true && currentOccupied == true)
+            return "|";
+        if (whitesTurn) {
+            if ((previousOccupied == false && currentOccupied == true))
+                return "[";
+            if ((previousOccupied == true && currentOccupied == false))
+                return "]";
+        } 
+        if ((previousOccupied == false && currentOccupied == true))
             return "]";
-        
-        return "|";
+        else 
+            return "[";
     }
 
 
