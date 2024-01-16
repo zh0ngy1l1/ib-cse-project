c5c7045e251084eb6e88d1be9451421e76335295
zh0ngy1l1 <zhongyi.david.li@gmail.com>
2024-01-16 14:16:27 +0200

    ok just wait

---
+++ Board.java
@@ -0,0 +1,51 @@
+public class Board {
+    protected String[][] board;
+    public boolean whitesTurn;
+
+    public Board(String FEN) {
+
+        String[] FEN_parts = FEN.split(" "); //"r3k2r/2p1npp1/1pnpbq1p/pB2p3/P3P3/2PPbN2/1PQN1PPP/R4RK1 w kq - 0 13"
+        
+        this.board = Utils.fenToBoard(FEN_parts[0]);
+        this.whitesTurn = FEN_parts[1].equals("w");
+    }
+
+    /**
+     * Display the board, with chess pieces,
+     */
+    public String displayBoard(boolean whitesTurn) {
+
+        String boardString = "  a b c d e f g h  \n";
+
+        for (int row = 8; row >= 1; row--) {
+            boardString += row;
+
+            for (int col = 1; col <= 8; col++){
+                ChessPiece piece = pieceAt(new Pair(row, col));
+
+                // if the sum of row + col is odd, the square is white.
+                String squareColor = ((row + col) & 1) == 1 ? Settings.ws : Settings.bs;
+
+                String overlaySeparator = " ";
+
+                boardString += overlaySeparator + (piece == null ? squareColor : piece);
+            }
+            
+            // now the last overlayboard separator
+            boardString += " ";
+            boardString += row + "\n";
+        }
+        boardString += "  a b c d e f g h  ";
+        
+        if (whitesTurn) return boardString;
+        else return new StringBuilder(boardString).reverse().toString();
+    }
+
+    public String pieceAt(Pair square) {
+        if (!Utils.isValidSquare(square)) {
+            throw new IllegalArgumentException("Square out of bounds");
+        }
+
+        return board[square.getFirst()][square.getSecond()];
+    }
+}
---
+++ Utils.java
@@ -0,0 +1,66 @@
+public class Utils {
+    /**
+     * Takes the FEN notation and makes it an array with 
+     * the row representing the rank and the column 
+     * representing the file (1 = a, etc)
+     */
+    public static String[][] fenToBoard(String FEN_board) {
+        String[][] board = new String[9][9];
+        String[] rows = FEN_board.split("/");
+        for (int row = 0; row < 8; row++) {
+            char[] rowCArr = rows[7 - row].toCharArray();
+            int fenCol = 0;
+
+            for (int boardCol = 0; boardCol < 8; boardCol++) {
+                char current = rowCArr[fenCol];
+
+                if (Character.isDigit(current)) {
+                    for (int q = 1; 
+                        q < Integer.parseInt(String.valueOf(current)); 
+                        q++) {
+
+                        board[row + 1][boardCol + 1] = " ";
+                        boardCol++;
+                    }
+                    board[row + 1][boardCol + 1] = " ";
+
+                } else board[row + 1][boardCol + 1] = charToPiece(current);
+
+                fenCol++;
+            }
+        }
+        return board;
+    }
+
+    /**
+     * Is square within the 8x8 chessboard?
+     */
+    public static boolean isValidSquare(Pair square) {
+        return (
+            !(square.getFirst() < 1 || square.getFirst() > 8 || 
+            square.getSecond() < 1 || square.getSecond() > 8
+        ));
+    }
+
+    /**
+     * Given the FEN character return the piece icon as string.
+     */
+    public static String charToPiece(char c) {
+        boolean isWhite = Character.isUpperCase(c) ? true : false;
+        c = Character.toLowerCase(c);
+
+        if (c == 'p') {
+            return (isWhite ^ Settings.lightMode) ? "♟︎" : "♙";
+        } if (c == 'n') {
+            return (isWhite ^ Settings.lightMode) ? "♞" : "♘";
+        } if (c == 'b') {
+            return (isWhite ^ Settings.lightMode) ? "♝" : "♗";
+        } if (c == 'r') {
+            return (isWhite ^ Settings.lightMode) ? "♜" : "♖";
+        } if (c == 'q') {
+            return (isWhite ^ Settings.lightMode) ? "♛" : "♕";
+        } else {
+            return (isWhite ^ Settings.lightMode) ? "♚" : "♔";
+        }
+    }
+}
